f5d36eca744d7f24d55c9c1556e7e030aa765d0b
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2022-12-08 15:44:21 +0700

    add_remember_token_for_user_table

--- database/migrations/2022_12_05_030607_create_users_table.php
+++ database/migrations/2022_12_05_030607_create_users_table.php
@@ -21,8 +21,8 @@ return new class extends Migration
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->tinyInteger('type');
-            $table->timestamp('deleted_at');
             $table->timestamps();
+            $table->rememberToken();
         });
     }
 
