27aa0202b2375f16d941a1acbc659b62fa7c7961
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2022-12-23 10:58:34 +0700

    one_return

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -19,23 +19,20 @@ class UserRepository
     {
         $query = $this->model->query();
         $keyword = $input['key'] ?? "";
-        $columnName = $input['column_name'] ?? "id";
-        $sortType = $input['sort_type'] ?? "asc";
 
         if ($keyword) {
-            return $query
-                ->where('name', 'LIKE', "%{$keyword}%")
+            $query->where('name', 'LIKE', "%{$keyword}%")
                 ->orWhere('id', 'LIKE', "%{$keyword}%")
-                ->orWhere('phone', 'LIKE', "%{$keyword}%")
-                ->orderBy($columnName, $sortType)
-                ->paginate(5);
+                ->orWhere('phone', 'LIKE', "%{$keyword}%");
         }
 
+        $columnName = $input['column_name'] ?? "id";
+        $sortType = $input['sort_type'] ?? "asc";
         $checkColumn = Schema::hasColumn('users', $columnName);
         $checkSortType = in_array(strtolower(trim($sortType)), static::SORT_TYPES);
-        
+
         if ($checkColumn && $checkSortType) {
-            return $query->orderBy($columnName, $sortType)->paginate(5);
+            $query->orderBy($columnName, $sortType);
         }
 
         return $query->paginate(5);
