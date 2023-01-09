eafdd53046c4ea604bf39ce588cb99548c4ad2e4
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-01-09 09:29:38 +0700

    fix

--- resources/assets/js/benefits.js
+++ resources/assets/js/benefits.js
@@ -1,9 +1,9 @@
-var myNodelist = document.getElementsByClassName("benefitLi");
-var benefitArray=[];
+let myNodelist = document.getElementsByClassName("benefitLi");
+let benefitArray=[];
 
-for (var i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+for (let i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
@@ -11,15 +11,15 @@ for (var i = 0; i < myNodelist.length; i++) {
 
 // Click on a close button to hide the current list item
 let close = document.getElementsByClassName("close");
-for (var i = 0; i < close.length; i++) {
+for (let i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-    var div = this.parentElement;
+    let div = this.parentElement;
     div.style.display = "none";
   }
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
+let list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.className === 'benefitLi') {
     ev.target.classList.toggle('checked');
@@ -28,9 +28,9 @@ list.addEventListener('click', function(ev) {
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
   li.appendChild(t);
   li.className="benefitLi";
 
@@ -43,15 +43,15 @@ function newElement() {
   benefitArray=benefitArray.concat(inputValue.split(','));
   document.getElementById("benefits").value=JSON.stringify(benefitArray);
   inputValue="";
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     }
   }
