226782edac2a4f365f46765de46c8c45845b3627
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-01-11 11:07:25 +0700

    list_course

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="d-flex ">
+                @foreach ($courses as $course)
+                    <div class="card mx-1" style="width: 18rem;">
+                        <img class="card-img-top h-50 "
+                            src="{{ URL::asset('storage/attachments/' . $course->attachment->file_name) }}"
+                            alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title text-uppercase text-truncate">{{ $course->name }}</h5>
+                            <p class="card-text mb-0">Price: {{ $course->price }} $</p>
+                            <p class="card-text mb-0">Old Price:<del class="text-danger"> {{ $course->old_price }}</del> $
+                            </p>
+                            <p class="card-text mb-0">Views: {{ $course->view_count ?? '0' }} <i class="fa-solid fa-eye"></i>
+                            </p>
+                            <p class="card-text mb-0 text-truncate">Description: {{ $course->description }}
+                            </p>
+                            <a href="#" class="btn btn-danger">Add to cart <i
+                                    class="fa-solid fa-cart-shopping"></i></a>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                    </div>
+                @endforeach
             </div>
+            <div class="mt-5">
+                {{ $courses->appends(request()->all())->links() }}</div>
         </div>
     </div>
-</div>
 @endsection
