16c99ddadd0b2c0af75b4e3b3164c8e085837ab1
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-01-30 17:03:26 +0700

    pint

--- app/Http/Controllers/Backend/CartController.php
+++ app/Http/Controllers/Backend/CartController.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Mail\CheckoutCart as MailCheckoutCart;
 use App\Models\Course;
-use App\Notifications\CheckoutCart;
 use App\Repositories\CourseRepository;
 use App\Services\CartService;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Mail;
 
 class CartController extends Controller
@@ -74,7 +72,7 @@ class CartController extends Controller
     }
     public function checkoutCart()
     {
-        Mail::to(request()->user())->send(new MailCheckoutCart(app(CartService::class)->getAll(),request()->total));
+        Mail::to(request()->user())->send(new MailCheckoutCart(app(CartService::class)->getAll(), request()->total));
         return redirect()->route('cart')->with('notification', 'Checkout cart successfully!!');
     }
 }
