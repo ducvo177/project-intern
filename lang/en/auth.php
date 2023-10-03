fa7f7fe60e97d20d112425a62a3737c2e44464ce
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-10-03 08:59:07 +0700

    fix_login

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -15,8 +15,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Tài khoản bạn nhập vào đã sai, vui lòng thử lại',
+    'password' => 'Mật khẩu bạn nhập vào sai',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
